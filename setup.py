6c4aefe91087e3c5f5c796813932b9a1c0255101
Twotiger <dgl0518@gmail.com>
2018-05-29 13:15:51 +0800

    v3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='dingding',
-    version='0.2',
+    version='0.3',
     author='twotiger',
     author_email='dgl0518@gmail.com',
     url='https://github.com/Twotiger/dingding',
