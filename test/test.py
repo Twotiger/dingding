f059640b02e1067a23081c041ffc6e7ce4af3430
Twotiger <dgl0518@gmail.com>
2020-12-03 16:16:37 +0800

    test(location):为测试添加延迟

--- test/test.py
+++ test/test.py
@@ -1,14 +1,17 @@
 # coding:utf-8
+import time
 from dingding import DingDing
 from collections import namedtuple
 import unittest
 
 TOKEN = ""
 PHONE = ""
+SECRET = ""
 
 
 class TestDingDing(unittest.TestCase):
     def test_parse_token(self):
+        time.sleep(1)
 
         TestDing = namedtuple("TestDing", ["input", "want"])
 
@@ -28,17 +31,23 @@ class TestDingDing(unittest.TestCase):
             self.assertEqual(d.url, test_ding.want)
 
     def test_send_text(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_text("天气不错")
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_text2(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_text("天气不错", [PHONE])
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_link(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_link(
             "天气提示",
             "天气不错",
@@ -48,12 +57,16 @@ class TestDingDing(unittest.TestCase):
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_markdown(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_markdown("天气提示", "# 天气不错\n> 雨一直下")
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_single_action_card(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_single_action_card(
             "乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身",
             "![screenshot](@lADOpwk3K80C0M0FoA) \n #### 乔布斯 20 年前想打造的苹果咖啡厅 \n\n Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划",
@@ -65,7 +78,9 @@ class TestDingDing(unittest.TestCase):
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_action_card(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_action_card(
             "新用户注册",
             "![screenshot](@lADOpwk3K80C0M0FoA) \n## 请及时审核",
@@ -74,7 +89,9 @@ class TestDingDing(unittest.TestCase):
         self.assertEqual(rep["errmsg"], "ok")
 
     def test_send_feed_card(self):
+        time.sleep(1)
         dingding = DingDing(TOKEN)
+        dingding.set_secret(SECRET)
         rep = dingding.send_feed_card(
             [
                 (
